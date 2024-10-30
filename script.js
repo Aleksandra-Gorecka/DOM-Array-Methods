0b1252cd84af7a0f43aded79cba60175a1ad4ca8
Aleksandra Sobiecka <aleks.sobiecka@gmail.com>
2024-10-30 16:33:17 +0100

    Added function sort() to sort users by richest

--- script.js
+++ script.js
@@ -36,6 +36,12 @@ function doubleMoney() {
   updateDOM();
 }
 
+// Sort users by richest
+function sortByRichest() {
+  data.sort((a, b) => b.money - a.money);
+  updateDOM();
+}
+
 // Add new obj to data arr
 function addData(obj) {
   data.push(obj);
@@ -69,3 +75,4 @@ function formatMoney(number) {
 // Event listeners
 addUserBtn.addEventListener("click", getRandomUser);
 doubleBtn.addEventListener("click", doubleMoney);
+sortBtn.addEventListener("click", sortByRichest);
