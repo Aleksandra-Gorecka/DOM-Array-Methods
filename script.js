9f2c46c5104711f7b3db27de7ca649b11169b3ea
Aleksandra Sobiecka <aleks.sobiecka@gmail.com>
2024-10-30 12:45:55 +0100

    Added JS for output users with forEach() and function to add new user

--- script.js
+++ script.js
@@ -30,4 +30,32 @@ async function getRandomUser() {
 // Add new obj to data arr
 function addData(obj) {
   data.push(obj);
+
+  updateDOM();
 }
+
+// Update DOM
+function updateDOM(providedData = data) {
+  // Clear the main div
+  main.innerHTML = "<h2><strong>Person</strong> Wealth</h2>";
+
+  providedData.forEach((item) => {
+    // Create new HTML element
+    const element = document.createElement("div");
+    element.classList.add("person");
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
+      item.money
+    )}`;
+
+    // Insert new element to the DOM
+    main.appendChild(element);
+  });
+}
+
+// Format nuber as money - https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
+function formatMoney(number) {
+  return "$ " + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
+}
+
+// Event listeners
+addUserBtn.addEventListener("click", getRandomUser);
